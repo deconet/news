cd3a5f8f1a25e93bc1a5065e7a1834afabe959f9
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2018-10-17 01:23:11 -0700

    rename landing page

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,14 @@
 class StoriesController < ApplicationController
   def index
+    # 50 items per page
+    items_per_page = 50
+    page_offset = params[:page].nil ? 0 : params[:page] * items_per_page
     render json: {
       stories: Story.all
         .includes(:story_source, :story_source_category)
         .order(story_time: :desc)
+        .offset(page_offset)
+        .limit(items_per_page)
         .as_json(methods: [
           :story_source,
           :comments_url
