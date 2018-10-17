4df661b2af65c479de450620b939756abca1c168
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2018-10-17 01:47:59 -0700

    filter hiring stories

--- lib/utils/yc_utils.rb
+++ lib/utils/yc_utils.rb
@@ -20,6 +20,7 @@ module YcUtils
     puts "getting #{url}"
     res = RestClient.get(url)
     story_data = JSON.parse(res.body)
+    return if story_data['descendants'].nil? # skip stories like blabla is hiring wtih no commetns
     puts "adding yc story #{story_data}"
     s = Story.find_or_create_by(id_at_source: story_data['id'], story_source_id: story_source.id)
     s.update(
